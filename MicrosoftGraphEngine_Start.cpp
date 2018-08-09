47d29f9d8e3317700800a5b29fc8f4485086e2bb
tungtokyo1108 <dangthanhtung91@gmail.com>
2018-08-09 22:24:02 +0900

    Add files via upload

---
+++ MicrosoftGraphEngine_Start.cpp
@@ -0,0 +1,16 @@
+/*
+ * MicrosoftGraphEngine_Start.cpp
+ *
+ *  Created on: Aug 9, 2018
+ *      Author: Tung Dang
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Let's to start" << endl;
+	return 0;
+}
+
+
