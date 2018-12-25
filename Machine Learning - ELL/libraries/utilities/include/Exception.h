7fb4d3f96bcc5ac22ca73c95c728066af6194f5d
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-12-25 11:07:20 +0900

    Add files via upload

---
+++ Machine Learning - ELL/libraries/utilities/include/Exception.h
@@ -0,0 +1,8 @@
+/**
+ * Microsoft - Modern Information Technology
+ * https://github.com/Microsoft/ELL/blob/master/libraries/utilities/include/Exception.h
+ *
+ *  Created on: Dec 25, 2018
+ *  Student (MIG Virtual Developer): Tung Dang
+ */
+
