00d896bae81a86fae11146c272a7317c469a1407
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-12-25 11:42:14 +0900

    Update Exception.h

--- Machine Learning - ELL/libraries/utilities/include/Exception.h
+++ Machine Learning - ELL/libraries/utilities/include/Exception.h
@@ -6,3 +6,101 @@
  *  Student (MIG Virtual Developer): Tung Dang
  */
 
+#include <exception>
+#include <string>
+
+namespace ell 
+{
+    namespace utilities
+    {
+        class Exception : public std::exception
+        {
+            public:
+            Exception(const std::string& message) : 
+                _message(message) {}
+
+            ~Exception() override = default;
+
+            virtual const std::string& GetMessage() const {return _message;}
+            const char* what() const noexcept override {return GetMessage().c_str();}
+
+            private:
+            std::string _message;
+        };
+
+        template <typename ErrorCodeType>
+        class ErrorCodeException : public Exception
+        {
+            public:
+            ErrorCodeException(ErrorCodeType errorCode, const std::string& message = "");
+
+            ErrorCodeType GetErrorCode() const noexcept {return _errorCode;};
+
+            private: 
+            ErrorCodeType _errorCode;
+        };
+
+        enum class LogicExceptionErrors
+        {
+            illegalState,
+            notImplemented,
+            notInitialized
+        };
+
+        enum class SystemExceptionErrors
+        {
+            fileNotFound,
+            fileNotWritable,
+            serialPortUnavailable
+        };
+
+        enum class NumericExceptionErrors
+        {
+            divideByZero,
+            overflow,
+            didNotConverge
+        };
+
+        enum class InputExceptionErrors
+        {
+            badStringFormat,
+            badData,
+            indexOutOfRange,
+            invalidArgument,
+            invalidSize,
+            nullReference,
+            sizeMismatch,
+            typeMismatch,
+            versionMismatch
+        };
+
+        enum class DataFormatErrors
+        {
+            badFormat,
+            illegalValue,
+            abruptEnd
+        };
+
+        using LogicException = ErrorCodeException<LogicExceptionErrors>;
+        using SystemException = ErrorCodeException<SystemExceptionErrors>;
+        using NumericException = ErrorCodeException<NumericExceptionErrors>;
+        using InputException = ErrorCodeException<InputExceptionErrors>;
+        using DataFormatException = ErrorCodeException<DataFormatErrors>;
+    }
+}
+
+#pragma region implementation 
+
+namespace ell 
+{
+    namespace utilities
+    {
+        template <typename ErrorCodeType>
+        ErrorCodeException<ErrorCodeType>::ErrorCodeException(ErrorCodeType errorCode, const std::string& message) : 
+            Exception(message),
+            _errorCode(errorCode)
+        {}
+    }
+}
+
+#pragma endregion implementation
