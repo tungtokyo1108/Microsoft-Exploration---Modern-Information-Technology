3748c57fc7ae9f508de217bec25790cf4b35317c
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-12-26 23:46:06 +0900

    Create testing.cpp

---
+++ Machine Learning - ELL/libraries/testing/src/testing.cpp
@@ -0,0 +1,7 @@
+/**
+ * Microsoft - Modern Information Technology
+ * https://github.com/Microsoft/ELL/blob/master/libraries/testing/src/testing.cpp
+ *
+ *  Created on: Dec 26, 2018
+ *  Student (MIG Virtual Developer): Tung Dang
+ */
