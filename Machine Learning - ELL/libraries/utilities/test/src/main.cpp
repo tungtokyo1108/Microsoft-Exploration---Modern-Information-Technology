75617448a20c25a31682314604d3b08e63ec3a43
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-12-27 23:51:27 +0900

    Run Files_test

---
+++ Machine Learning - ELL/libraries/utilities/test/src/main.cpp
@@ -0,0 +1,43 @@
+/**
+ * Microsoft - Modern Information Technology
+ * https://github.com/Microsoft/ELL/blob/master/libraries/utilities/test/src/main.cpp
+ *
+ *  Created on: Dec 27, 2018
+ *  Student (MIG Virtual Developer): Tung Dang
+ */
+
+#include "utilities/test/include/Files_test.h"
+#include <testing/include/testing.h>
+#include <utilities/include/Exception.h>
+#include <utilities/include/Files.h>
+
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <thread>
+#include <vector>
+
+using namespace ell;
+
+int main(int argc, char const *argv[])
+{
+    try 
+    {
+        std::string basePath = ell::utilities::GetDirectoryPath(argv[0]);
+        TestStringf();
+        TestJoinPaths(basePath);
+        #ifdef WIN32
+        TestUnicodePaths(basePath);
+        #endif
+    }
+    catch (const utilities::Exception& exception)
+    {
+        std::cerr << "Error, got ELL exception. Message:" << exception.GetMessage() << std::endl;
+        throw;
+    }
+    if (testing::DidTestFail())
+    {
+        return 1;
+    }
+    return 0;
+}
