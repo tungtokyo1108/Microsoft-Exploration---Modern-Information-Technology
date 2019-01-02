3e09728b9c0231da2e5616383fd0ce3112a124fa
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-01-02 22:51:45 +0900

    Update CStringParser.h

--- Machine Learning - ELL/libraries/utilities/include/CStringParser.h
+++ Machine Learning - ELL/libraries/utilities/include/CStringParser.h
@@ -104,6 +104,249 @@ namespace ell
             errno = tmp;
             return ParseResult::success;
         }
+
+        inline ParseResult cParse(const char* pStr, char*& pEnd, unsigned int& value)
+        {
+            if (!IsDigit(*pStr))
+            {
+                return ParseResult::badFormat;
+            }
+
+            auto tmp = errno;
+            errno = 0;
+
+            auto x = strtoul(pStr, &pEnd, 0);
+            if (x != static_cast<unsigned int>(x))
+            {
+                return ParseResult::outOfRange;
+            }
+
+            value = static_cast<unsigned int>(x);
+
+            if (pStr == pEnd)
+            {
+                return ParseResult ::badFormat;
+            }
+            if (errno == ERANGE)
+            {
+                return ParseResult::badFormat;
+            }
+
+            errno = tmp;
+            return ParseResult::success;
+        }
+
+        inline ParseResult cParse(const char* pStr, char*& pEnd, uint64_t& value)
+        {
+            if (!IsDigit(*pStr))
+            {
+                return ParseResult::badFormat;
+            }
+
+            auto tmp = errno;
+            errno = 0;
+
+            auto x = strtoul(pStr, &pEnd, 0);
+            if (pStr == pEnd)
+            {
+                return ParseResult::badFormat;
+            }
+            if (errno == ERANGE)
+            {
+                return ParseResult::outOfRange;
+            }
+            if (x != static_cast<uint64_t>(x))
+            {
+                return ParseResult::outOfRange;
+            }
+
+            value = static_cast<uint64_t>(x);
+            errno = tmp;
+            return ParseResult::success;
+        }
+
+        inline ParseResult cParse(const char* pStr, char*& pEnd, int& value)
+        {
+            if (IsWhiteSpace(*pStr))
+            {
+                return ParseResult::badFormat;
+            }
+
+            auto tmp = errno;
+            errno = 0;
+
+            auto x = strtol(pStr, &pEnd, 0);
+            if (pStr == pEnd)
+            {
+                return ParseResult::badFormat;
+            }
+            if (errno == ERANGE)
+            {
+                return ParseResult::outOfRange;
+            }
+            value = static_cast<int>(x);
+            errno = tmp;
+            return ParseResult::success;
+        }
+
+        inline ParseResult cParse(const char* pStr, char*& pEnd, long& value)
+        {
+            if (IsWhiteSpace(*pStr))
+            {
+                return ParseResult::badFormat;
+            }
+
+            auto tmp = errno;
+            errno = 0;
+            value = strtol(pStr, &pEnd, 0);
+
+            if (pStr == pEnd)
+            {
+                return ParseResult::badFormat;
+            }
+            if (errno == ERANGE)
+            {
+                return ParseResult::outOfRange;
+            }
+
+            errno = tmp;
+            return ParseResult::success;
+        }
+
+        inline ParseResult cParse(const char* pStr, char*& pEnd, unsigned short& value)
+        {
+            if (!IsDigit(*pStr))
+            {
+                return ParseResult::badFormat;
+            }
+
+            auto tmp = errno;
+            errno = 0;
+
+            auto x = strtoul(pStr, &pEnd, 0);
+
+            if (pStr == pEnd)
+            {
+                return ParseResult::badFormat;
+            }
+            if (errno == ERANGE)
+            {
+                return ParseResult::outOfRange;
+            }
+            if (x != static_cast<unsigned short>(x))
+            {
+                return ParseResult::outOfRange;
+            }
+            value = static_cast<unsigned short>(x);
+
+            errno = tmp;
+            return ParseResult::success;
+        }
+
+        inline ParseResult cParse(const char* pStr, char*& pEnd, short& value)
+        {
+            if (IsWhiteSpace(*pStr))
+            {
+                return ParseResult::badFormat;
+            }
+
+            auto tmp = errno;
+            errno = 0;
+
+            long x = strtol(pStr, &pEnd, 0);
+
+            if (pStr == pEnd)
+            {
+                return ParseResult::badFormat;
+            }
+            if (errno == ERANGE)
+            {
+                return ParseResult::outOfRange;
+            }
+            if (x != static_cast<short>(x))
+            {
+                return ParseResult::outOfRange;
+            }
+
+            value = static_cast<short>(x);
+
+            errno = tmp;
+
+            return ParseResult::success;
+        }
+
+        inline ParseResult cParse(const char* pStr, char*& pEnd, char& value)
+        {
+            value = *pStr;
+            pEnd = const_cast<char*>(++pStr);
+            return ParseResult::success;
+        }
+
+        inline ParseResult cParse(const char* pStr, char*& pEnd, std::string& value)
+        {
+            const char* iter = pStr;
+            while (std::isalnum(*iter) || *iter == '_')
+            {
+                ++iter;
+            }
+            value = std::string(pStr,iter);
+            pEnd = const_cast<char*>(iter);
+            return ParseResult::success;
+        }
+
+        template<typename std::enable_if_t<!std::is_same<unsigned long, unsigned int>::value, int> = 0>
+        inline ParseResult cParse(const char* pStr, char*& pEnd, unsigned long& value)
+        {
+            if (!IsDigit(*pStr))
+            {
+                return ParseResult::badFormat;
+            }
+
+            auto tmp = errno;
+            errno = 0;
+
+            value = strtoul(pStr, &pEnd, 0);
+
+            if (pStr == pEnd)
+            {
+                return ParseResult::badFormat;
+            }
+            if (errno == ERANGE)
+            {
+                return ParseResult::outOfRange;
+            }
+
+            errno = tmp;
+            return ParseResult::success;
+        }
+
+        template <typename ValueType>
+        ParseResult Parse(const char*& pStr, ValueType& value)
+        {
+            if (IsEndOfString(*pStr))
+            {
+                return ParseResult::endOfString;
+            }
+
+            if(*pStr == '/')
+            {
+                if (*(pStr + 1) == '/')
+                {
+                    return ParseResult::beginComment;
+                }
+            }
+
+            if (*pStr == '#')
+            {
+                return ParseResult::beginComment;
+            }
+
+            char* pEnd = nullptr;
+            auto parseResult = cParse(pStr, pEnd, value);
+            pStr = pEnd;
+
+            return parseResult;
+        }
     }
 }
 
