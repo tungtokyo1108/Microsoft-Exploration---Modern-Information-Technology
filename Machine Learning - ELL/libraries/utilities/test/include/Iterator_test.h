7805289f50f905647450ec170026e7587c0997a2
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-01-20 21:27:13 +0900

    Add files via upload

---
+++ Machine Learning - ELL/libraries/utilities/test/include/Iterator_test.h
@@ -0,0 +1,16 @@
+/**
+ * Microsoft - Modern Information Technology
+ * https://github.com/Microsoft/ELL/blob/master/libraries/utilities/test/include/Iterator_test.h
+ *
+ *  Created on: Jan 16, 2019
+ *  Student (MIG Virtual Developer): Tung Dang
+ */
+
+namespace ell
+{
+void TestIteratorAdapter();
+void TestTransformIterator();
+void TestParallelTransformIterator();
+
+void TestStlStridedIterator();
+} 
