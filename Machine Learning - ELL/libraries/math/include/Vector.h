ae725b27f8f6797bba4f28d85b02e00d0513a784
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-02-08 11:37:55 +0900

    Create Vector.h

---
+++ Machine Learning - ELL/libraries/math/include/Vector.h
@@ -0,0 +1,7 @@
+/**
+ * Microsoft - Modern Information Technology
+ * https://github.com/tungtokyo1108/ELL/blob/master/libraries/math/include/Vector.h
+ *
+ *  Created on: Feb 07, 2019
+ *  Student (MIG Virtual Developer): Tung Dang
+ */
