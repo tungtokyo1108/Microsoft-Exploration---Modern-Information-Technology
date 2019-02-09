bef90f598fc58c65b64af1861be8ecfc873231f7
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-02-10 00:33:03 +0900

    Create Common.h

---
+++ Machine Learning - ELL/libraries/math/include/Common.h
@@ -0,0 +1,25 @@
+/**
+ * Microsoft - Modern Information Technology
+ * https://github.com/Microsoft/ELL/blob/master/libraries/math/include/Common.h
+ *
+ *  Created on: Feb 10, 2019
+ *  Student (MIG Virtual Developer): Tung Dang
+ */
+
+#pragma once 
+
+namespace ell 
+{
+    namespace math
+    {
+        enum class ImplementationType 
+        {
+            native,
+            openBlas
+        };   
+
+        struct One
+        {};
+        
+    } 
+}
