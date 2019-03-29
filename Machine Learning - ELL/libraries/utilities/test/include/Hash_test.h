e73a3d1b65a3906fe647b6b723b19a80c77230f7
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2019-03-29 23:39:58 +0900

    Create Hash_test.h

---
+++ Machine Learning - ELL/libraries/utilities/test/include/Hash_test.h
@@ -0,0 +1,13 @@
+/**
+ * Microsoft - Modern Information Technology
+ * https://github.com/Microsoft/ELL/blob/master/libraries/utilities/test/include/Hash_test.h
+ *
+ *  Created on: Mar 29, 2019
+ *  Student (MIG Virtual Developer): Tung Dang
+ */
+
+#pragma once 
+
+namespace ell {
+    void Hash_test1();
+}
